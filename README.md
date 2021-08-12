17e9d661f9d764605764309addd3e092c7547d63
zzlwte <zzlwte@gmail.com>
2021-08-12 14:31:31 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,58 @@
+[![npm][npm]][npm-url]
+[![node][node]][node-url]
+[![GitHub issues](https://img.shields.io/github/issues/zzlw/html-webpack-cdn.svg)](https://github.com/zzlw/html-webpack-cdn/issues)
+[![GitHub forks](https://img.shields.io/github/forks/zzlw/html-webpack-cdn.svg)](https://github.com/zzlw/html-webpack-cdn/network)
+[![GitHub stars](https://img.shields.io/github/stars/zzlw/html-webpack-cdn.svg)](https://github.com/zzlw/html-webpack-cdn/stargazers)
+[![GitHub license](https://img.shields.io/github/license/zzlw/html-webpack-cdn.svg)](https://github.com/zzlw/html-webpack-cdn/blob/main/LICENSE)
+
+<div align="center">
+  <img width="200" height="200" src="https://worldvectorlogo.com/logos/html5.svg">
+  <a href="https://github.com/webpack/webpack">
+    <img width="200" height="200"
+      src="https://webpack.js.org/assets/icon-square-big.svg">
+  </a>
+  <div>
+    <img width="100" height="100" title="Webpack Plugin" src="http://michael-ciniawsky.github.io/postcss-load-plugins/logo.svg">
+  </div>
+  <h1>html-webpack-cdn</h1>
+  <p>无需配置 webpack externals 自动向 html-webpack-plugins 模板注入 unpkg cdn</p>
+</div>
+
+<h2 align="center">Install</h2>
+
+<h3>Webpack 5</h3>
+
+```bash
+  npm i --save-dev html-webpack-cdn
+```
+
+```bash
+  yarn add --dev html-webpack-cdn
+```
+
+```js
+// 无需配置 webpack externals
+// modules 为空则不会生效
+const WebpackHtmlCdnPlugin = require("html-webpack-cdn");
+
+const cdnEnv = process.env.NODE_ENV === 'development' ? 'development' : 'production.min';
+
+new HtmlWebpackCdnPlugin({
+  modules: [
+    { name: 'react', _var: 'React', path: `umd/react.${cdnEnv}.js` },
+    { name: 'react-dom', _var: 'ReactDOM', path: `umd/react-dom.${cdnEnv}.js` },
+  ],
+})
+```
+
+```html
+<!-- html 中效果 -->
+<script src="https://unpkg.com/react@17.0.2/umd/react.production.min.js"></script>
+<script src="https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js"></script>
+```
+
+[npm]: https://img.shields.io/npm/v/html-webpack-cdn.svg
+[npm-url]: https://npmjs.com/package/html-webpack-cdn
+
+[node]: https://img.shields.io/node/v/html-webpack-cdn.svg
+[node-url]: https://nodejs.org
---
+++ index.js
@@ -0,0 +1,114 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// 获取 包 版本号
+const getModulesVersion = () => {
+  let mvs = {};
+  let regexp = /^npm_package_.{0,3}dependencies_/gi;
+  for (let m in process.env) {
+    // 从node内置参数中读取，也可直接import 项目文件进来
+    if (regexp.test(m)) {
+      // 匹配模块
+      // 获取到模块版本号
+      mvs[m.replace(regexp, "").replace(/_/g, "-")] = process.env[m].replace(
+        /(~|\^)/g,
+        ""
+      );
+    }
+  }
+  return mvs;
+};
+
+const package_version = getModulesVersion();
+
+let cdnSource = [];
+
+// 默认 cdn 供应商
+const UNPKG = "UNPKG";
+
+const cdnSourcePaths = {
+  [UNPKG]: `https://unpkg.com`,
+};
+
+const isEmpty = (rest = {}) => Object.values(rest).includes(undefined);
+
+const getSourcePath = ({ packageName, version, path, cdnType }) => {
+  if (cdnType === UNPKG) {
+    return `${cdnSourcePaths[UNPKG]}/${packageName}@${version}/${path}`;
+  }
+};
+
+// 默认配置
+const defaultConfig = {
+  cdnType: UNPKG,
+};
+
+class WebpackExternalsCdnPlugin {
+  constructor({ modules = [], ...config } = {}) {
+    this.modules = modules;
+    this.config = { ...defaultConfig, ...config };
+  }
+  apply(compiler) {
+    const webpackExternals = (compiler.options.externals =
+      compiler.options.externals || {});
+
+    // 没有 externals return
+    if (JSON.stringify(webpackExternals) == "{}" && !this.modules.length)
+      return;
+
+    if (
+      Object.prototype.toString.call(webpackExternals) === "[object Object]"
+    ) {
+      this.modules.forEach((item) => {
+        const { name, _var, path } = item;
+        if (isEmpty({ name, _var, path })) {
+          console.error(
+            "\x1B[31m%s\x1B[0m",
+            `未匹配 CDN ${name}, 请检查参数 WebpackExternalsCdnPlugin modules`
+          );
+          return;
+        }
+        webpackExternals[item.name] = item._var;
+      });
+    }
+
+    compiler.hooks.compilation.tap(
+      "WebpackExternalsCdnPlugin",
+      (compilation, compilationParams) => {
+        Object.keys(webpackExternals).forEach(async (packageName) => {
+          // 已装载过 cdn
+          if (cdnSource.some((item) => item.name === packageName)) return;
+          // 获取包的版本号
+          const version = package_version[packageName];
+          // modules cdn
+          const path = this.modules.find(
+            (item) => item.name === packageName
+          )?.path;
+          const initPath = getSourcePath({
+            packageName,
+            version,
+            path,
+            ...this.config,
+          });
+          cdnSource.push({
+            name: packageName,
+            path: initPath,
+          });
+        });
+        HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync(
+          "WebpackExternalsCdnPlugin",
+          (data, cb) => {
+            cdnSource.forEach((item) => {
+              data.headTags.push({
+                tagName: "script",
+                voidTag: false,
+                attributes: { defer: false, src: item.path },
+              });
+            });
+            cb(null, data);
+          }
+        );
+      }
+    );
+  }
+}
+module.exports = WebpackExternalsCdnPlugin;
